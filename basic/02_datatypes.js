d60aae2169a3f1165518e53a91640dccb94ad90e
CodeMaestrop <154520210+CodeMaestrop@users.noreply.github.com>
2024-01-05 18:14:32 +0000

    datatypes

---
+++ basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newly version
+
+// alert("hello")//we are usi5ng node,not browser5
+console.log(3+3);
+console.log("Tanisha")
+let name="Tanisha"
+let age=18
+let isLoggedIn=false
+
+
+
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value
+//undifined
+//symbol=unique
+console.log(typeof "Tanisha");
+console.log(typeof null);
+console.log(typeof undefined);
