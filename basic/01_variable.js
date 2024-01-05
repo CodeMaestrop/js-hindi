f343b3a7a59e865624dd7d5c29038053d11a3289
CodeMaestrop <154520210+CodeMaestrop@users.noreply.github.com>
2024-01-05 18:05:09 +0000

    variable

---
+++ basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 144553
+let accountEmail = "Tanisha@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID = 3
+accountEmail="hc@hc.com";
+acccountPassword="87654"
+accountCity="khaga";
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
