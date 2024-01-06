32eff83a1469ba1ed8b7e17b16772766a3be0e2b
CodeMaestrop <154520210+CodeMaestrop@users.noreply.github.com>
2024-01-06 19:47:10 +0000

    conver

--- basic/03conversionopr.js
+++ basic/03conversionopr.js
@@ -1,25 +1,57 @@
-// let score = true
+// // let score = true
 
-// console.log(typeof score);
-// console.log(typeof(score));
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-//"33"=33
-//"33abc"=NaN
-//true=1,false=0
+// //"33"=33
+// //"33abc"=NaN
+// //true=1,false=0
 
-// let isLoggedIn=1
+// // let isLoggedIn=1
 
-// let booleanIsLoggedIn=Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
-//1=true,0=false
-//""=false
-//"Tanisha"=true
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
-console.log(typeof someNumber);
+// // let booleanIsLoggedIn=Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
+// //1=true,0=false
+// //""=false
+// //"Tanisha"=true
+// // let someNumber = 33
+// // let stringNumber = String(someNumber);
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+// // console.log(typeof someNumber);
+
+// //operation
+
+// //  let value=33;
+
+// //  let negVal=-value
+// //  console.log(negVal);
+
+// //  console.log(2+2);
+// //  console.log(2-2);
+// //  console.log(2*2);
+// //  console.log(2**3);
+// //  console.log(2%2);
+// //  console.log(2^2);
+ 
+
+// let str1="hello"
+// let str2=" Tanisha"
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
